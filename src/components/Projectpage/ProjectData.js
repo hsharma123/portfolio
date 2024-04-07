ae3ba2e34c07d2858c963e65596156b467ce5a39
hsharma123 <bhs985576@gmail.com>
2024-04-07 17:28:09 +0530

    changes are update

---
+++ src/components/Projectpage/ProjectData.js
@@ -0,0 +1,69 @@
+import acma from '../../assets/Images/acma.jpg';
+import Lincoln from '../../assets/Images/Lincoln.jpg';
+import Genesis from '../../assets/Images/genesis.jpg';
+import Dap from '../../assets/Images/dial-a-physio.jpg';
+import Das from '../../assets/Images/DAS.jpg';
+import wbs from '../../assets/Images/wbs.jpg';
+import youtube from '../../assets/Images/youtube.jpg';
+import Case from '../../assets/Images/Case.jpg';
+import ecommerce from '../../assets/Images/ecommerce.png';
+
+const projectData = [
+    {
+        imgPath: Case,
+        title: "Case Converter",
+        description: " This website proper in react framework. Whether you need to convert your text to uppercase, lowercase, title case, sentence case, or even camel case, CaseConverter Website has got you covered.",
+        ghLink: "https://himanshu-case.netlify.app/",
+    },
+    {
+        imgPath: youtube,
+        title: "Youtube Clone",
+        description: "Created youtube clone using the react framework and tailwind css framework also use axios to create a new card using rapid api and fully responsive. use state management for manage the state.",
+        ghLink: "https://tumate.netlify.app/",
+    },
+    {
+        imgPath: ecommerce,
+        title: 'Ecommerce Website',
+        description: 'This is an ecommerce website. It features a user-friendly interface, product catalog, checkout process. The website also includes features  wishlists, and product filter, price filter, enhance the overall shopping experience. also react technology use in this website.',
+        ghLink: "https://himanshuecomerce.netlify.app/",
+      },
+    {
+        imgPath: acma,
+        title: "Acma",
+        description: "The Automotive Component Manufacturers Association of India (ACMA) is the apex body representing the interest of the Indian Auto Component Industry...",
+        ghLink: "https://www.acma.in/",
+    },
+    {
+        imgPath: Lincoln,
+        title: "Lincoln University",
+        description: "Lincoln University of Business and Management is a private institution based in Sharjah, United Arab Emirates, with campuses in Sharjah and Fujairah. LUBM offers management programs in bachelor, master and diploma levels in association with international institutions.",
+        ghLink: "https://lincoln-edu.ae/",
+    },
+    {
+        imgPath: Genesis,
+        title: "Genesis",
+        description: "Genesis Finance Company Limited is a leading Indian Financial Institution specializing in providing debt financing to MSMEs and consumers in India. In August 2018.",
+        ghLink: "https://genesisfinance.net/",
+    },
+    {
+        imgPath: Dap,
+        title: "Dial-a-Physio",
+        description: "Dial-A-Physio is a In Home and Clinic based Physiotherapy and rehabilitation facility. We provide a wide array of Physiotherapy services, at our Bolton , as well as in the comfort of your home.",
+        ghLink: "https://dialaphysio.com/",
+    },
+    {
+        imgPath: Das,
+        title: "Dehumidifiedairservices",
+        description: "As the manufacturer-direct OEM service provider of the factory that built your Dectron, PoolPak, and Seresco dehumidifier, nobody knows your dehumidifier like we do – because we’re the experts.",
+        ghLink: "https://dehumidifiedairservices.com/",
+    },
+    {
+        imgPath: wbs,
+        title: "Webeesocial Canada",
+        description: "We are WeBeeSocial, a creative digital marketing agency based out of Toronto that thrives on creating marketing campaigns that are not only digitally rich, but also creatively appealing.",
+        ghLink: "https://webeesocial.ca/",
+    },
+    // Add more project data objects here
+];
+
+export default projectData;
